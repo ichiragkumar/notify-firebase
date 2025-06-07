3f711bcbf70a2f1e701cacf36f7e632301a6f114
ichiragkumar <chiragroy3030@gmail.com>
2025-06-07 16:25:09 +0530

    Seeting up basic aapp with firebase notification

---
+++ src/config/firebase.ts
@@ -0,0 +1,7 @@
+import * as admin from 'firebase-admin';
+
+admin.initializeApp({
+  credential: admin.credential.cert(require('../../firebase-service-account.json')),
+});
+
+export const messaging = admin.messaging();
---
+++ src/config/handlers/fcm-error-handler.ts
@@ -0,0 +1,10 @@
+
+import { logger } from '../logger';
+export const handleFcmError = (error: any, token: string) => {
+  if (error.code === 'messaging/registration-token-not-registered' || error.code === 'messaging/invalid-registration-token') {
+    logger.warn({ token, code: error.code }, 'Invalid token, should be removed');
+    // Here you would remove the token from DB if implemented
+  } else {
+    logger.error({ error }, 'Failed to send push notification');
+  }
+};
