c6b13ca39e020ef54550f2e759e429a7e8a93c16
ichiragkumar <chiragroy3030@gmail.com>
2025-06-07 18:36:29 +0530

    implmented push one to one push notfication

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,12 @@
 import * as admin from 'firebase-admin';
+import * as path from 'path';
+
+const serviceAccountPath = path.resolve(__dirname, '../../firebase-service-account.json');
+
 
 admin.initializeApp({
-  credential: admin.credential.cert(require('../../firebase-service-account.json')),
+  credential: admin.credential.cert(serviceAccountPath),
+
 });
 
 export const messaging = admin.messaging();
--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,2 +1,23 @@
 import pino from 'pino';
-export const logger = pino({ transport: { target: 'pino-pretty' } });
+import pretty from "pino-pretty";
+
+
+
+const stream = pretty({
+  levelFirst: true,
+  colorize: true,
+  ignore: "time,hostname,pid",
+});
+
+export const logger = pino(
+  {
+    name: "MyLogger",
+    level: process.env.NODE_ENV === "development" ? "debug" : "info",
+  },
+  stream
+);
+
+
+
+
+
